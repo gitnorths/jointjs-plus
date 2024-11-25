4d232570bf27d24ec98666a26efc7f8c301f2926
lzc <1196132438@qq.com>
2024-11-25 10:57:26 +0800

    项目初始化bat

---
+++ npmInstall.bat
@@ -0,0 +1,7 @@
+set http_proxy=http://localhost:7890
+set https_proxy=http://localhost:7890
+set ELECTRON_MIRROR=https://npmmirror.com/mirrors/electron/
+set ELECTRON_BUILDER_BINARIES_MIRROR=https://npmmirror.com/mirrors/electron-builder-binaries/
+set SASS_BINARY_SITE=https://npmmirror.com/mirrors/node-sass/
+set phantomjs_cdnurl=https://mirrors.huaweicloud.com/phantomjs/
+npm i
